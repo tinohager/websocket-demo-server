15391793bf512712ed4e53e502738cf34ada6aec
Tino Hager <tino.hager@nager.at>
2024-01-10 13:43:46 +0100

    Initial commit

---
+++ src/WebsocketDemoServer/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.WebSockets;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddWebSockets(config => { });
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseWebSockets();
+
+app.Map("/ws", async context =>
+{
+    var buffer = new byte[1024 * 4];
+
+
+    WebSocketReceiveResult result = null;
+
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        using (var webSocket = await context.WebSockets.AcceptWebSocketAsync())
+        {
+            var receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            while (!receiveResult.CloseStatus.HasValue)
+            {
+                var text = Encoding.UTF8.GetString(buffer.ToArray(), 0, buffer.Length);
+                Console.WriteLine(text);
+
+                await webSocket.SendAsync(Encoding.ASCII.GetBytes($"Received - {DateTime.Now}"), WebSocketMessageType.Text, true, CancellationToken.None);
+
+                receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+        }
+    }
+    else
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+    }
+});
+
+await app.RunAsync();
+
+//app.UseAuthorization();
+//app.MapControllers();
+
+//app.Run();
